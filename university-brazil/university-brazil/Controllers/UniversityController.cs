90ed085004980871605d4c4ad7f0e2304aeda8df
Isaac Rodrigues <ibra.isaac@gmail.com>
2021-10-18 16:10:02 -0300

    first commit.

---
+++ university-brazil/university-brazil/Controllers/UniversityController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using university_brazil.Models;
+
+namespace university_brazil.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UniversityController : Controller
+    {
+        private HttpClient client;
+        private Uri uri;
+        private List<UniversityModel> universityModels = null;
+
+        public UniversityController()
+        {
+            universityModels = new List<UniversityModel>();
+            if (client == null)
+            {
+                client = new HttpClient();
+                client.BaseAddress = new Uri("http://universities.hipolabs.com/");
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            }
+
+            InitializeApi();
+        }
+
+        private async void InitializeApi()
+        {
+            HttpResponseMessage response = client.GetAsync("search?country=brazil").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                uri = response.Headers.Location;
+
+                universityModels = await JsonSerializer.DeserializeAsync<List<UniversityModel>>(await response.Content.ReadAsStreamAsync());
+            }
+        }
+
+        [HttpGet]
+        public IEnumerable<UniversityModel> Get()
+        {
+            if (universityModels.Count == 0)
+            {
+                InitializeApi();
+            }
+
+            return universityModels.ToList();
+        }
+    }
+}
---
+++ university-brazil/university-brazil/Models/UniversityModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace university_brazil.Models
+{
+    public class UniversityModel
+    {
+        public string alpha_two_code { get; set; }
+
+        public IEnumerable<string> web_pages { get; set; }
+
+        public string name { get; set; }
+
+        public string country { get; set; }
+
+        public IEnumerable<string> domains { get; set; }
+
+        public string state_province { get; set; }
+    }
+}
